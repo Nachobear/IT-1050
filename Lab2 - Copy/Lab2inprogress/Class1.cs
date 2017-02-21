7c403db9ab808b9eb1930b35aefaa825cbf78765
Nachobear <kainerob@gmail.com>
2017-02-21 14:47:42 -0500

    2nd lab
    2nd lab

---
+++ Lab2 - Copy/Lab2inprogress/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2inprogress
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+        public static int Count;
+
+        public static double SumOfAllAges;
+        public static double AverageAge()
+        {
+            return (double)SumOfAllAges / (double)Person.Count;
+        }
+
+        public string GetFullName()
+        {
+            string fullname = " " + FirstName + " " + LastName;
+            return fullname;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(" " + FirstName + " " + LastName + " is " + Age + " years old.");
+        }
+
+        public void AskUserForNameAndAgeP1()
+        {
+            System.Console.WriteLine("What is person1's first name?");
+            FirstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is person1's last name?");
+            LastName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is person1's age?");
+            Age = int.Parse(System.Console.ReadLine());
+
+            Person.Count++;
+            Person.SumOfAllAges += Age;
+        }
+
+        public void AskUserForNameAndAgeP2()
+        {
+            System.Console.WriteLine("What is person2's first name?");
+            FirstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is person2's last name?");
+            LastName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is person2's age?");
+            Age = int.Parse(System.Console.ReadLine());
+
+            Person.Count++;
+            Person.SumOfAllAges += Age;
+        }
+
+        public void AskUserForNameAndAgeSpouse()
+        {
+            System.Console.WriteLine("What is their spouse's first name?");
+            FirstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is their spouse's last name?");
+            LastName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is their spouse's age?");
+            Age = int.Parse(System.Console.ReadLine());
+
+            Person.Count++;
+            Person.SumOfAllAges += Age;
+        }
+
+        public void CreateSpouseIfMarried()
+        {
+            System.Console.WriteLine("Are they married? (y/n)");
+            if(System.Console.ReadLine().ToLower().StartsWith("y"))
+            {
+                Spouse = new Person();
+                Spouse.AskUserForNameAndAgeSpouse();
+                Spouse.Spouse = this;
+            }
+        }
+    }
+}
---
+++ Lab2 - Copy/Lab2inprogress/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2inprogress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person P1 = new Person();
+            P1.AskUserForNameAndAgeP1();
+            P1.CreateSpouseIfMarried();
+
+            Person P2 = new Person();
+            P2.AskUserForNameAndAgeP2();
+            P2.CreateSpouseIfMarried();
+
+            P1.PrintNameAndAge();
+            P1.Spouse.PrintNameAndAge();
+            P2.PrintNameAndAge();
+            P2.Spouse.PrintNameAndAge();
+            System.Console.WriteLine(" Average Age: " + Person.AverageAge());
+            System.Console.ReadKey();
+        }
+    }
+}
